55baef3f7a20711cbd623f729c8b00726f76fd8f
levani-hacker <114279610+levani-hacker@users.noreply.github.com>
2023-07-30 14:38:16 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var Levani_age = 13;
+var Arudashvili_age = 15;
+var Tyeshela_age = 10;
+
+
+let Levani = "ლევან წაქაძე";
+let Nika = "არუდაშვილი ნიკა";
+let Dato = "დათო ტყეშელაშვილი";
+
+
+if (Levani_age >= 18){
+    console.log(Levani + "სრულწლოვანი ხარ");
+} else if (Levani_age >= 13){
+    console.log(Levani + "თინეიჯერი ხარ!");
+}else {
+    console.log(Levani + "ბღარი ხარ ჯერ ბიძი");
+} 
+
+
+if (Arudashvili_age >= 18){
+    console.log(Nika + "სრულწლოვანი ხარ");
+} else if (Arudashvili_age>= 13){
+    console.log(Nika + "თინეიჯერი ხარ!");
+}else {
+    console.log(Nika + "ბღარი ხარ ჯერ ბიძი");
+} 
+
+
+
+if (Tyeshela_age >= 18){
+    console.log(Dato + "სრულწლოვანი ხარ");
+} else if (Tyeshela_age >= 13){
+    console.log(Dato + "თინეიჯერი ხარ!");
+}else {
+    console.log(Dato + "ბღარი ხარ ჯერ ბიძი");
+} 
+
